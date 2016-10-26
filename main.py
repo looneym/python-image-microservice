342ebf7068c6987a9f8639f4de0b3b5c6f8b9e5c
Micheál Looney <hello@micheallooney.com>
2016-10-26 01:08:38 +0100

    Swiched to using CDN for image hosting

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import os
 from flask import Flask, request
 import json
 import sys
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 app = Flask(__name__)
+
+cloudinary.config(
+      cloud_name = 'imgrab',
+      api_key = '',
+      api_secret = ''
+    )
 
 def download_file(name, url):
     # TODO - Exception handling for 404 and no directory
@@ -14,14 +23,18 @@ def download_file(name, url):
 
 @app.route('/',methods=['POST'])
 def foo():
+   print request.data
    data = json.loads(request.data)
    print data
    name = data['name']
    url = data['url']
 
    try:
-        download_file(name, url)
-        return "OK"
+        # download_file(name, url)
+        cloudinary.uploader.upload(str(url), public_id =str(name))
+        print cloudinary.utils.cloudinary_url(str(name)+".jpg")
+        return cloudinary.utils.cloudinary_url(str(name)+".jpg")
+
    except:
        # you sunk my battleship
         print "Unexpected error:", sys.exc_info()[0]
